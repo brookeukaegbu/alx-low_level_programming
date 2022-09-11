2f9c9012028905fa6018da55bbc9ec9a5c92ca2b
“Brooke <brookeukaegbu@gmail.com>
2022-09-11 08:26:07 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-puts ("Programming is like building a multilingual puzzle) return 0;
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 If successful
+ *
+ * Description: This prints "Programming is like building a multilingual
+ * puzzle
+ */
+
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+}
