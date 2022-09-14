c7b5a796877e4716178d9422cbba656ef8f16919
“Brooke <brookeukaegbu@gmail.com>
2022-09-14 14:10:18 -0700

    Task 13

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	/while (i < 10)/
-	/{/
-	/putchar(i);/
-	/}/
+	while
+	/ (i < 1) /
+	/ { /
+	/ putchar(i); /
+	/ } /
 
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
