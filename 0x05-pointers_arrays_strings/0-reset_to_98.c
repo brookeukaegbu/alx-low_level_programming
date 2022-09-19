6d9e5c9e62be7471676264b21d37c862cab8d859
“Brooke <brookeukaegbu@gmail.com>
2022-09-19 08:16:07 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * reset_to_98 - takes an integer pointer and
- * updates the value it points to to 98
- * @n: an integer pointer that stores the address argument
- * Return: returns nothing
+ * swap_int - function that swaps two intege
+ * @a: frist integer
+ * @b: second integer
+ * Return: nothing
  *
  */
 
