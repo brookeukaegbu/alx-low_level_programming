e85720d78a81e0758d318acb5d967dec94e22cfa
“Brooke <brookeukaegbu@gmail.com>
2022-09-21 16:30:09 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i;
 
-	while (op == 0)
+	for (i = 0; s1[i] == s2[i]; i++)
 	{
-		if (((s1 + i) == '\0') && ((s2 + i) == '\0'));
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		if (s1[i] == 0)
+			return (0);
 	}
-	return (op);
+	return (s1[i] - s2[i]);
 }
