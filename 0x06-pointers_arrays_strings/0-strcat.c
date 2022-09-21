938dd896fa51775009dfee4dfd0cfc09617b8679
“Brooke <brookeukaegbu@gmail.com>
2022-09-21 15:47:47 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - appends src to dest string
+ * @dest: string that src will be appended on
+ * @src: string to append on dest
+ * Return: address of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c, d;
+
+	c = d = 0;
+	while (*(dest + c))
+		c++;
+	while ((*(dest + c) = *(src + d))
+			{
+			c++;
+			d++;
+			}
+			return (dest);
+}
+
