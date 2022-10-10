b59f5a5a4e2b870af01d1e15e5109a2f1867637b
“Brooke <brookeukaegbu@gmail.com>
2022-10-10 15:00:42 -0700

    program that prints the name of the file it was compiled from, followed by a new line

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main - prints the name of the file.
-  *
-  * Return: Always 0.
+ * main - a program that prints the name of the file it was compiled from
+ * Return: returns 0 (success)
  */
+
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
+
 	return (0);
 }
