3059578ee38a03ec030fb4b29e98abc936da3ab8
“Brooke <brookeukaegbu@gmail.com>
2022-10-12 13:06:16 -0700

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- *
- * Return: no return.
+ * print_name - a function that prints a name
+ * @name: the name to be printed
+ * @f: a pointer to a funtion that accepts a string as
+ * parameter and returns nothing
+ * Return: returns nothing
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
